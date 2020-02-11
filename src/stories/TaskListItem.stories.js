b906e253121b6bebb53c87e8ccfeb5ab17561769
kathleenhung5 <kathleenhung5@gmail.com>
2020-02-10 21:59:19 -0800

    Added Knobs.

--- src/stories/TaskListItem.stories.js
+++ src/stories/TaskListItem.stories.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import '../App.scss';
 import TaskListItem from '../comp/TaskListItem/index.js';
+import {withKnobs, boolean,text} from '@storybook/addon-knobs';
 
 export default {
     title: 'TaskListItem',
-    component: TaskListItem
+    component: TaskListItem,
+    decorators: [withKnobs]
 }
 
 export const DefaultTaskListItem = () => (
     <TaskListItem />
 );
 
-export const TaskWithAlertWithoutTag = () => (
-    <TaskListItem name='My Task' alert={true} tag=""/>
+export const CustomTaskListItem = () => (
+    <TaskListItem 
+    name={text('My Task')} alert={boolean("show alert",true)} tag={text("")}/>
 )
