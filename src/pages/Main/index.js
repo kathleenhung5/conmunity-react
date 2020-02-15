953cf65e95dbcc75675e9b70e5c1a071182132b2
kathleenhung5 <kathleenhung5@gmail.com>
2020-02-15 15:10:33 -0800

    Completed <AddButton />

---
+++ src/pages/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Main(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+Main.defaultProps = {
+
+}
+
+export default Main;
---
+++ src/pages/Projects/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Projects(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+Projects.defaultProps = {
+    
+}
+
+export default Projects;
