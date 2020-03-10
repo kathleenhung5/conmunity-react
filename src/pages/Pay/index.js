addab8261a799cbf5cfe85e81285ad6cdce63d60
kathleenhung5 <kathleenhung5@gmail.com>
2020-03-10 15:24:22 -0700

    Set up stripe and put in card element

--- src/pages/Pay/index.js
+++ src/pages/Pay/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import {loadStripe} from '@stripe/stripe-js';
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+} from '@stripe/react-stripe-js';
 
+const stripePromise = loadStripe('pk_test_33cyFX5JhyAdytACyYPI6cx00020aRNCHz');
 
 function Pay(){
+
+    // Form 
+    const CardForm = () => {
+        const stripe = useStripe();
+        const elements = useElements();
+        const handleSubmit = async (event) => {
+            event.preventDefault();        
+            const {error, paymentMethod} = await stripe.createPaymentMethod({
+                type: 'card',
+                card: elements.getElement(CardElement),
+              });
+              if (error) {
+                console.log('[error]', error);
+              } else {
+                console.log('[PaymentMethod]', paymentMethod);
+                localStorage.setItem('paid',true);
+              }       
+        };
+
+        return (
+            <form className="pay-form" onSubmit={handleSubmit}>
+                <h3>Card Information</h3>
+               <CardElement />
+               <button type="submit" disabled={!stripe}>Pay</button>
+            </form>
+      )
+    }
+
     return(
-        <div>
-            
-        </div>
+        <Elements stripe={stripePromise}>
+            <CardForm />
+        </Elements>
     )
 }
 
